b2f0bc887ec7570841073cc47c6ad366851b8acf
mak6969 <74461188+mak6969@users.noreply.github.com>
2021-01-18 17:39:51 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?
+    ini_set('display_errors', 'Off');
+ error_reporting(0);
+    session_start();
+
+    $_SESSION['time'] = time();
+
+    header("Refresh: 5; URL=https://link-to.net/165411/cheatooo");
+    print("Powered by KeyAuth.com login system");
+?>
+
+
+<html>
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<meta charset="utf-8">
+
+
+<title>Snowwy.wtf</title>
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="MainDesign.css"/>
+<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+
+<div class="keysyscontainer">
+    <div class="box">
+        <span class="title">Snowwy.wtf</span>
+        <span class="text"> You will have to go through four linkvertises. The key will last around 10 minutes. Redirecting in 5 seconds... </span>
+        <span class="text"> No one likes advertisments, but going through this supports the creators of Snowwy.wtf. </span>
+</html>
