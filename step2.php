72508945fef003b2ae5964af45987d6e24cefd14
mak6969 <74461188+mak6969@users.noreply.github.com>
2021-01-18 20:21:54 -0500

    Update step2.php

--- step2.php
+++ step2.php
@@ -23,14 +23,14 @@ print("Powered by KeyAuth.com login system");
 <meta charset="utf-8">
 
 
-<title>Snowwy.wtf</title>
+<title>KeySystem</title>
 <meta charset="utf-8" />
 <link rel="stylesheet" type="text/css" href="MainDesign.css"/>
 <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
 
 <div class="keysyscontainer">
     <div class="box">
-        <span class="title">Snowwy.wtf | Step 2</span>
+        <span class="title">KeySystem | Step 2</span>
         <span class="text"> You have completed one linkvertise, two more to go! Redirecting in 5 seconds... </span>
-        <span class="text"> No one likes advertisments, but going through this supports the creators of Snowwy.wtf. </span>
+        <span class="text"> No one likes advertisments, but going through this supports the creators of KeySystem. </span>
 </html>
